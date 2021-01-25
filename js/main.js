502d8b42ca847d9c5e9134a163402ac3e6443a83
truman0125 <70584937+truman0125@users.noreply.github.com>
2021-01-25 17:49:17 +0900

    01/25 update
    go

--- js/main.js
+++ js/main.js
@@ -1,99 +1,151 @@
 //// 롯데콘서트홀 JS - main.js /////
 
 ///// 로드구역 //////////////////////////////
-window.addEventListener("DOMContentLoaded",
-    function () {
-        //console.log("로딩완료!");
-    
-    // 이동버튼 클릭시 슬라이드 넘기기 /////
-    // 클릭 이벤트 대상: .abtn
-    // 이동버튼
-    var btn = document.querySelectorAll('.abtn');
-    //console.log("버튼개수:"+btn.length);
-    
-    // 왼쪽버튼
-    btn[0].onclick = function(){
-        //console.log("왼쪽이양!");
+    $(function(){
         
-        // 이동함수 호출
-        goSlide(0);
+        var sld = $("#slide");
+        var sldCnt = sld.find("li").length;
         
-        // a태그 기본이동 막기!
-        return false;
         
-    };
+        var sldSts = 0;//슬라이드 상태값
+        // 0 자동상태, 1 멈춤상태
         
-    // 오른쪽버튼
-    btn[1].onclick = function(){
-        //console.log("오른쪽이양!");
-        
-        // 이동함수 호출
-        goSlide(1);
-        
-        // a태그 기본이동 막기!
-        return false;
-        
-    };/////// click ////////////////
-        
-    // 페이지번호
-    var snum = 0;//첫페이지는 0
-    /*//////////////////////////////
-        함수명: goSlide
-        기능: 슬라이드가 넘어가도록 기능 구현
-    *///////////////////////////////
-    var goSlide = function(dir){
-        
-        //console.log("방향구분:"+dir);
-        
-        // 버튼별 슬라이드번호 증감!
-        if(dir){// dir이 1일때 true: 오른쪽 버튼
-            snum++;//1씩증가
-            if(snum===5) snum = 0;//한계수
+        $(".cir_off li").first().click(function(){
             
-        }///////// if /////////////////
-        else{// dir이 0일때: 왼쪽버튼
-            snum--;//1씩감소
-            if(snum<0) snum=4;//한계수
-        }///// else /////////////////
+            var v1,v2;
+            if(sldSts===0){
+                v1=1;
+                v2=0;
+                
+                clearInterval(autoI);
+                
+                
+            } 
+            else{
+                v1=0;
+                v2=1;
+                
+                autoSlide();
+                
+            }
+            
+            $("img:last-child",this).css({
+                opacity: v1
+            });
+            
+            $("img:first-child",this).css({
+                opacity: v2
+            });
+            
+            // 상태값 변경
+            sldSts ? sldSts = 0 : sldSts = 1;            
+        });////// click //////////////////////
         
-        console.log("슬라이드번호:"+snum);
         
-        // 슬라이드 이동하기
-        var tg = document.querySelector('#slide');
-        tg.style.left = (-100*snum)+"%";
-        tg.style.transition = "left .8s ease-out";
+        $(".cir_off li").click(function(){
+            
+            var idx = $(this).index();
+            console.log("fff"+idx);
+            if(idx===0) return false;
+            
+            
+            idx = idx-1;
+            console.log("seq:"+idx);
+            
+            clearAuto();
+            
+            sldFn(idx);
+            
+            // 전역변수값을 중간이동순번으로 업데이트!
+            snum = idx;
+            
+        });///////// click //////////////
         
-        // 3.블릿변경하기
-        // 원리: 슬라이드 순번과 같은 순서의 블릿li에
-        // class="on"을 줘서 회색이미지가 보이도록 한다!
-        // 블릿박스: .indic li
-        var indic = document.querySelectorAll('.indic li');
+        var sldFn = function(idx){
+            sld.animate({
+                left:(-100*idx)+"%"
+            },600);
+            
+            $(".cir_off li").eq(idx+1).addClass("on")
+            .siblings().removeClass("on");
+            
+        };//////////// sldFn //////////
         
-        // 블릿 li에 class초기화(모두지우기)
-        for(var x of indic){
-            x.classList.remove("on")
-        }//// for of 문 /////////////
-        
-        //  슬라이드 순번과 같은 블릿 li에 class 넣기
-        indic[snum].classList.add("on");
-        
-    };//////// goSlide함수 //////////////
-    ////////////////////////////////////
-        
-        
-    
-    /*//////////////////////////////
+    // 슬라이드전역변수    
+    var snum = 0;    
+         // 인터발함수(지우기위해 만든변수)
+    var autoI;
+    /*////////////////////////////////////////////
         함수명: autoSlide
         기능: 자동넘기기 셋팅함수(인터발함수)
-    *///////////////////////////////
-    var autoSlide = function(){};
-    
-    /*//////////////////////////////
+    */ ////////////////////////////////////////////
+    var autoSlide = function () {
+        autoI = setInterval(function () {
+            snum++;
+            if(snum===sldCnt)snum=0;
+            
+            sldFn(snum);
+            
+        }, 3000);
+        
+    }; ////// autoSlide함수 //////////////////////
+    //////////////////////////////////////////////
+
+    // 할당형함수 아래에서 처음 호출해야함!
+    autoSlide(); //최초호출!
+
+
+    // 타임아웃변수(쓰나미방지용 지우기변수)
+    var autoT;
+    /*////////////////////////////////////////////
         함수명: clearAuto
         기능: 자동넘김 지우기함수(클리어인터발함수)
-    *///////////////////////////////
-    var clearAuto = function(){};
+    */ ////////////////////////////////////////////
+    var clearAuto = function () {
 
-    }); /////////////// 로드구역 ///////////////////////
+        // 자동넘김 할당된 변수를 지운다(autoI)
+        clearInterval(autoI);
+        // 타임아웃 실행쓰나미 방지용 타임아웃 지우기를 꼭한다!
+        clearTimeout(autoT);
+
+        // 안건드리면 5초후 다시 자동호출하기!
+        // 매번 호출될때 위에서 지우므로 단 하나만 남아있음!
+        autoT = setTimeout(autoSlide, 5000);
+
+    }; ////// clearAuto함수 /////////////////////////
+    ///////////////////////////////////////////////
+
+
+
+
+        
+        
+       
+       
+      /* $("#btn01").click(function(){
+           //console.log("나야나1");
+           $("#slide img").attr("src","images/big01.jpg")
+       });////// click /////////
+       $("#btn02").click(function(){
+           //console.log("나야나2");
+           $("#slide img").attr("src","images/big02.jpg")
+       });////// click /////////
+       $("#btn03").click(function(){
+           //console.log("나야나3");
+           $("#slide img").attr("src","images/big03.jpg")
+       });////// click /////////
+       $("#btn04").click(function(){
+           //console.log("나야나4");
+           $("#slide img").attr("src","images/big04.jpg")
+       });////// click /////////
+       $("#btn05").click(function(){
+           //console.log("나야나5");
+           $("#slide img").attr("src","images/big05.jpg")
+       });////// click /////////
+        
+        */
+  
+     
+    });/////////////// 로드구역 ///////////////////////
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
